a0ac05776f799f4fbd4e1e9c5476f5087cac6e8c
wadallace <77767227+wadallace@users.noreply.github.com>
2024-07-15 19:09:19 -0400

    exercise 12 completed

--- exercises/12-use-effect/src/App.jsx
+++ exercises/12-use-effect/src/App.jsx
@@ -1,38 +1,59 @@
+/* eslint-disable semi */
 // Import useEffect here
-import "./App.css";
-import ronSwansonImage from "./assests/ronswanson.png";
+import { useEffect, useState } from 'react'
+import './App.css'
+import ronSwansonImage from './assests/ronswanson.png'
+import axios from 'axios'
 // import Axios (or use Fetch)
 
 function App() {
-  /**
-   * Set up your state
-   */
+  const [quote, setQuote] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [hasError, setHasError] = useState(false)
 
-  /**
-   * Make an AJAX call with the useEffect hook
-   */
+  const getQuote = async () => {
+    setIsLoading(true)
+    try {
+      const { data } = await axios.get(
+        'https://ron-swanson-quotes.herokuapp.com/v2/quotes'
+      )
+      setQuote(data)
+    } catch (error) {
+      setHasError(true)
+      console.log(error)
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getQuote()
+  }, [])
+
+
 
   return (
-    <body className="bg-warning text-center">
+    <body className='bg-warning text-center'>
       <img
         src={ronSwansonImage}
-        alt=""
-        width="450"
-        height="423"
-        className="mt-4"
+        alt=''
+        width='450'
+        height='423'
+        className='mt-4'
       />
-      <div className="container">
-        {/* Display a loading message */}
-        {/* Display an error message if the API fails */}
+      <div className='container'>
+        {isLoading && <div className='text-center'>Loading...</div>}
+        {hasError && (
+          <div className='text-center error'>Error loading quote</div>
+        )}
         <blockquote
-          id="quote"
-          className="blockquote bg-dark text-white border-0 mb-4"
+          id='quote'
+          className='blockquote bg-dark text-white border-0 mb-4'
         >
-          {/* Complete me */}
+          {quote}
         </blockquote>
       </div>
     </body>
-  );
+  )
 }
 
-export default App;
+export default App
